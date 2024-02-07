648af62c749b6cf1fb4839243e4e41a6e83599cf
Marco_Ivantschitz <ivantschitz@hotmail.com>
2024-02-07 21:46:46 +0100

    docs: add Javadoc documentation #7

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/repository/NoteRepository.java
@@ -6,6 +6,17 @@ import org.springframework.data.repository.CrudRepository;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
+/**
+ * Repository interface for managing notes.
+ */
 public interface NoteRepository extends CrudRepository<Note, String> {
+
+    /**
+     * Finds notes by content.
+     *
+     * @param content The content to search for in the notes.
+     * @return A list of notes containing the provided content.
+     * @throws IllegalArgumentException if the provided content is null.
+     */
     List<Note> findByContentContaining(String content);
 }
