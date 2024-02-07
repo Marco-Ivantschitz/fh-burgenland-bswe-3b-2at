1b6bb04bc3da56ff1f4d87c403d433e060e1856b
Marco-Ivantschitz <114686574+Marco-Ivantschitz@users.noreply.github.com>
2024-02-07 21:17:22 +0100

    docs: javadoc for noteservice and personservice interfaces #5 (#9)
    * docs: add Javadoc documentation #5
    
    * docs: add Javadoc documentation #5

--- src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteService.java
+++ src/main/java/io/muehlbachler/fhburgenland/swm/examination/service/NoteService.java
@@ -5,10 +5,32 @@ import java.util.Optional;
 
 import io.muehlbachler.fhburgenland.swm.examination.model.Note;
 
+/**
+ * Service interface for managing notes.
+ */
 public interface NoteService {
+
+    /**
+     * Retrieves a note by its ID.
+     *
+     * @param id The ID of the note to retrieve.
+     * @return Optional containing the retrieved note, if found; otherwise, an empty Optional.
+     */
     Optional<Note> get(String id);
 
+    /**
+     * Creates a new note.
+     *
+     * @param note The note object to create.
+     * @return The created note object.
+     */
     Note create(Note note);
 
+    /**
+     * Queries notes by content.
+     *
+     * @param query The query string to search for in the notes' content.
+     * @return A list of notes matching the query. If no matching notes are found, an empty list is returned.
+     */
     List<Note> queryByContent(String query);
 }
